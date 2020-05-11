21142676265f426c96078ee7d58ec848d3d665ad
Aleksey Melnikov <melnikkov@gmail.com>
2020-05-12 01:04:02 +0300

    Update 1.py

--- Training/1.py
+++ Training/1.py
@@ -1,5 +1,13 @@
-String = "123456789101112"
-String1 = String[:-4]
-String2 = String[-4:-1]
-print(String1)
-print(String2)
+Strings = "6767879988787867576"
+
+def maskifi(String):
+    if len(String) > 4:
+        String1 = String[:-4]
+        String2 = String[-4:]
+        String3 = len(String1)
+        String4 = String1.replace(String1, "#" * String3) + String2
+        print(String4)
+    else:
+        print(String)
+
+maskifi(Strings)
