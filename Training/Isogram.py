eff884c814c5c600808adc0c9c1a93fb1cebfdd8
Aleksey <melnikkov@gmail.com>
2020-05-12 16:39:03 +0300

    1

--- Training/Isogram.py
+++ Training/Isogram.py
@@ -13,11 +13,6 @@ print(isogram(string))
 
 string = "Dismental"
 s = set(string)
-<<<<<<< HEAD
-if s == list(string):
-    print(True)
-=======
 l = {string}
 c = s - l
 print(c)
->>>>>>> 540ee52c366249c948a8932c58116bccba9f4766
