540ee52c366249c948a8932c58116bccba9f4766
Aleksey Melnikov <melnikkov@gmail.com>
2020-05-12 16:23:08 +0300

    Update Isogram.py

--- Training/Isogram.py
+++ Training/Isogram.py
@@ -1,4 +1,18 @@
-string = "ara"
+string = "Dismental"
+
+def isogram(string):
+    s = set(string)
+    for i in s:
+        c = string.count(i)
+        if c > 1:
+            return False
+
+
+print(isogram(string))
+
+
+string = "Dismental"
 s = set(string)
-if s == string:
-    print(True)
+l = {string}
+c = s - l
+print(c)
