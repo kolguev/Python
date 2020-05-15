26f4775467b81d99f45b770599e66278ddd554d0
Aleksey <melnikkov@gmail.com>
2020-05-15 19:45:51 +0300

    update final FindEvenIndex

--- Training/FindEvenIndex.py
+++ Training/FindEvenIndex.py
@@ -1,14 +1,8 @@
 def find_even_index(arr):
-    Number = 0
-    for i in arr:
-        if sum(arr[0:Number]) == sum(arr[(Number + 1):]):
-            return Number
-        Number += 1
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[(i + 1):]):
+            return i
     return -1
 
 ListOfInteger = [20,10,30,10,10,15,35]
 print(find_even_index(ListOfInteger))
-
-#Sum0 = sum(ListOfInteger[0:3])
-#Sum1 = sum(ListOfInteger[(3 + 1):len(ListOfInteger)])
-#print(Sum0 == Sum1)
