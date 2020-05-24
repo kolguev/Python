04912b1022729927d0dee00e54f68f4c8962e43a
Aleksey <melnikkov@gmail.com>
2020-05-24 13:37:34 +0300

    -

--- Training/any.py
+++ Training/any.py
@@ -1,4 +1,18 @@
-import sys, requests
+from PIL import Image
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
+
+# number_list = []
+# for number in range(1100, 5, -58):
+#     number_list.append(number)
+# print(number_list)
+
+# word = "letters"
+# print(set(word))
+# letter_counts = {letter: word.count(letter) for letter in set(word)}
+# print(letter_counts)
+
+
+img = Image.open("1.png")
+img.format
