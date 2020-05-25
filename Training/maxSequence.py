1c6d2d0a69035d51b4e63f3e878aa40f022e2a3c
Aleksey <melnikkov@gmail.com>
2020-05-26 00:14:44 +0300

    update

--- Training/maxSequence.py
+++ Training/maxSequence.py
@@ -1,7 +1,8 @@
 # maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 # should be 6: [4, -1, 2, 1]
-s = [sum(l[ind:x]) for x in range(1, len(l) + 1)]
-d = {s: l[ind:x]}
+
+# s = [sum(l[ind:x]) for x in range(1, len(l) + 1)]
+# d = {s: l[ind:x]}
 
 l = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 ind = 0
