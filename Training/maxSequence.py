c3526bb2e5117e6c0dce95e3fd9d5e8acd2bbd24
Aleksey <melnikkov@gmail.com>
2020-05-26 16:29:58 +0300

    update finish

--- Training/maxSequence.py
+++ Training/maxSequence.py
@@ -1,15 +1,28 @@
+# codewars.com kata Maximum subarray sum
 # should be 6: [4, -1, 2, 1]
 
+# l = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 l = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
 
 def maxSequence(arr):
     arr2 = []
-    for ind in range(0, len(arr)):
-        arr2.append(
-            max([[sum(arr[ind:x]), arr[ind:x]] for x in range(ind + 1, len(arr) + 1)])
-        )
-    return ": ".join([str(elem) for elem in max(arr2)])
+    if arr:
+        for ind in range(0, len(arr)):
+            arr2.append(
+                max(
+                    [
+                        [sum(arr[ind:x]), arr[ind:x]]
+                        for x in range(ind + 1, len(arr) + 1)
+                    ]
+                )
+            )
+        if max(arr2)[0] > 0:
+            return max(arr2)[0]
+        else:
+            return 0
+    else:
+        return 0
 
 
 print(maxSequence(l))
