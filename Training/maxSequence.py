10c9a3919f6908937c1f7395aabe19bb6d8ebba9
Aleksey <melnikkov@gmail.com>
2020-05-26 16:46:24 +0300

    update

--- Training/maxSequence.py
+++ Training/maxSequence.py
@@ -19,10 +19,12 @@ def maxSequence(arr):
 
 print(maxSequence(l))
 
-def MaxSequence(arr):
-    max,curr=0,0
-    for x in arr:
-        curr+=x
-        if curr<0:curr=0
-        if curr>max:max=curr
-    return max
+# def MaxSequence(arr):
+#     max,curr=0,0
+#     for x in arr:
+#         curr+=x
+#         if curr<0:
+#             curr=0
+#         if curr>max:
+#             max=curr
+#     return max
