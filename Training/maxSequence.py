4eecb44e18c2ecff683da37d16023c902c0c2d09
Aleksey <melnikkov@gmail.com>
2020-05-26 15:07:31 +0300

    update

--- Training/maxSequence.py
+++ Training/maxSequence.py
@@ -3,11 +3,15 @@
 
 l = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
-# for ind in range(0, len(l)):
-ind = 0
-SumList = [sum(l[ind:x]) for x in range(ind + 1, len(l) + 1)]
+l2 = []
+for ind in range(0, len(l)):
+    # ind = 3
+    # SumList = [sum(l[ind:x]) for x in range(ind + 1, len(l) + 1)]
 
-AnyList = [[sum(l[ind:x]), l[ind:x]] for x in range(ind + 1, len(l) + 1)]
-ListOfSequences = [l[ind:x] for x in range(ind + 1, len(l) + 1)]
-# print(AnyList)
-print(AnyList)
+    l2.append(max([[sum(l[ind:x]), l[ind:x]] for x in range(ind + 1, len(l) + 1)]))
+
+    ListOfSequences = [l[ind:x] for x in range(ind + 1, len(l) + 1)]
+    # print(AnyList)
+    # print(max([sum(l[ind:x]) for x in range(ind + 1, len(l) + 1)]))
+
+print(max(l2))
