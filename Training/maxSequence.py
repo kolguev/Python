78044ac406909b81e371c5d91aba5ee154dff6a5
Aleksey <melnikkov@gmail.com>
2020-05-26 15:39:08 +0300

    finish code

--- Training/maxSequence.py
+++ Training/maxSequence.py
@@ -1,17 +1,15 @@
 # should be 6: [4, -1, 2, 1]
 
-maxSequence = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+l = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
-def maxSequence(arr)
-maxSequence2 = []
-for ind in range(0, len(maxSequence)):
-    maxSequence2.append(
-        max(
-            [
-                [sum(maxSequence[ind:x]), maxSequence[ind:x]]
-                for x in range(ind + 1, len(maxSequence) + 1)
-            ]
+
+def maxSequence(arr):
+    arr2 = []
+    for ind in range(0, len(arr)):
+        arr2.append(
+            max([[sum(arr[ind:x]), arr[ind:x]] for x in range(ind + 1, len(arr) + 1)])
         )
-    )
+    return ": ".join([str(elem) for elem in max(arr2)])
 
-print(": ".join([str(elem) for elem in max(maxSequence2)]))
+
+print(maxSequence(l))
