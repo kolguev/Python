e53cfe6a140bf92889e8077cdb09607d17797dfc
Aleksey <melnikkov@gmail.com>
2020-05-28 12:56:53 +0300

    update

--- Training/count_smileys.py
+++ Training/count_smileys.py
@@ -3,17 +3,21 @@
 
 import re
 
-l =  [';]', ':[', ';*', ':$', ';-)']
+l =  [';D', ':-(', ':-)', ';~)']
 
 # for x in range(len(l)):
 #     if l[x].find(';-D') == 0:
 #         print(x)
 
-for x in l:
-    result = re.findall(r'[;|:][-|~]?[)|D]$', x)
-    if result:
-       print(result)
+def count_smileys(arr):
+    Count = []
+    for x in l:
+        result = re.findall(r'[;|:][-|~]?[)|D]$', x)
+        if result:
+            Count.append(result)
+    return len(Count)
 
+count_smileys(l)
 #print(",".join(l))
 
 # result = re.search("i", 'rfirjirfrjfijrf:)')
