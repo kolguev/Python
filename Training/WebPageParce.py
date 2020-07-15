b518082f2846939402079efb0ca6a23e75c2af4a
Aleksey Melnikov <melnikkov@gmail.com>
2020-07-15 20:14:34 +0300

    Update WebPageParce.py

--- Training/WebPageParce.py
+++ Training/WebPageParce.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 r = requests.get('https://www.sport-express.ru/')
+soup = BeautifulSoup(r.content.decode('utf-8'), 'lxml')
 t = r.text
 print(t)
 print(r.status_code)
