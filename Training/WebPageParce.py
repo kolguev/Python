cee5766a590a5e4cef47f49f6f603794cfeb4813
Aleksey <melnikkov@gmail.com>
2020-07-16 01:34:14 +0300

    -

--- Training/WebPageParce.py
+++ Training/WebPageParce.py
@@ -1,7 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-r = requests.get('https://www.sport-express.ru/')
-t = r.text
-print(t)
-print(r.status_code)
+HeadersKey = {
+    "accept": "aplication/json",
+    "email": "alexseym@inbox.ru",
+    "password": "qwe123!",
+}
+
+GetKey = requests.get("http://petfriends1.herokuapp.com/api/key", headers=HeadersKey)
+Key = GetKey.text
+AuthKey = Key[(Key.find(":") + 2) : (len(Key) - 3)]
+print(Key)
+print(AuthKey)
+
+HeadersPets = {"accept": "aplication/json", "auth-key": AuthKey}
+
+# print(HeadersPets)
+
+GetMyPets = requests.get(
+    "http://petfriends1.herokuapp.com/api/pets", headers=HeadersPets
+)
+Pets = GetMyPets.text
+print(Pets[0:200])
+
