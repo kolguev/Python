930a21088c8357b971e4c806aea5ed0a1ced8dc8
Aleksey Melnikov <melnikkov@gmail.com>
2020-07-16 13:28:58 +0300

    -

--- Training/WebPageParce.py
+++ Training/WebPageParce.py
@@ -1,6 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import cgi
+
+form = cgi.FieldStorage()
+uri = form.getfirst("uri", "not defined")
+
+p1k = form.getfirst("param1-key", "not defined")
+p1v = form.getfirst("param1-value", "not defined")
+
+p2k = form.getfirst("param2-key", "not defined")
+p2v = form.getfirst("param2-value", "not defined")
+
+p3k = form.getfirst("param3-key", "not defined")
+p3v = form.getfirst("param3-value", "not defined")
+
+print("Content-type: text/html\n")
+print("""<!DOCTYPE HTML>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Обработка данных форм</title>
+        </head>
+        <body>""")
+
+print("<h1>Обработка данных форм!</h1>")
+print("<p>uri: {}</p>".format(uri))
+print("<p>param1-key: {}</p>".format(p1k))
+print("<p>param1-value: {}</p>".format(p1v))
+print("<p>param2-key: {}</p>".format(p2k))
+print("<p>param2-value: {}</p>".format(p2v))
+print("<p>param3-key: {}</p>".format(p3k))
+print("<p>param3-value: {}</p>".format(p3v))
+
+
+print("""</body>
+        </html>""")
+
+
+######################################
 
 HeadersKey = {
     "accept": "aplication/json",
