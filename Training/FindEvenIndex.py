93c3fc7ac1674a371a1b3dcd282f4b8130255d75
Aleksey <melnikkov@gmail.com>
2020-08-17 11:17:58 +0300

    delete some files

--- Training/FindEvenIndex.py
+++
@@ -1,8 +0,0 @@
-def find_even_index(arr):
-    for i in range(len(arr)):
-        if sum(arr[:i]) == sum(arr[(i + 1):]):
-            return i
-    return -1
-
-ListOfInteger = [20,10,30,10,10,15,35]
-print(find_even_index(ListOfInteger))
