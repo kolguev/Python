f7fdd8a404bc1a9b9a8b7bd9058fa3153cd7b893
Aleksey Melnikov <melnikkov@gmail.com>
2020-08-20 01:35:14 +0300

    Update any.py

--- Training/any.py
+++ Training/any.py
@@ -19,7 +19,7 @@ tristique neque. Sed luctus mi ut nisi suscipit placerat. Nunc nec diam dapibus,
 Integer non magna molestie nibh dapibus tincidunt. Quisque quis est quam. Sed dictum mi sit amet magna pretium blandit. 
 Nulla tortor turpis, maximus vitae lobortis quis, varius sed metus. Nullam at congue metus. Pellentesque scelerisque, dui 
 et luctus semper, odio diam scelerisque justo, nec tempor ex metus et enim. Praesent rhoncus nisl eget risus elementum ornare. 
-Praesent tellus mauris, viverra vitae malesuada at, ornare id nisi. Vestibulum."""
+Praesent tellus mauris, viverra vitae malesuada at, ornare id nisi. Vestibulumi."""
 
 text = text.replace(" ", "").lower()
 most_common = None
@@ -29,11 +29,9 @@ l = []
 
 for item in set(text):
     qty = text.count(item)
-    print(item)
-    print(qty)
     l.append(qty)
-    #d.update(item, qty)
-print(sorted(l))
-l = sorted(l)
-print(l[len(l) - 2 : -1])
+    d.update({qty: item})
 
+l = sorted(l)
+needLetter = (l[len(l) - 2 : -1])
+print(d[needLetter[0]])
