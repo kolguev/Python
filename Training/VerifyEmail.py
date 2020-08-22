7144da5090f562f259c5339eef3d40016ebe6b9f
Aleksey <melnikkov@gmail.com>
2020-08-22 03:00:15 +0300

    update VerifyEmail.py

--- Training/VerifyEmail.py
+++ Training/VerifyEmail.py
@@ -4,10 +4,10 @@ mail = input("Input email: ").split(", ")
 
 dict_email = {}
 for i in mail:
-    result = re.findall(r"(^\w\w+[@]\w\w+[\.][a-z][a-z]+$)", i)
+    result = re.findall(r"(^\w\w+[@]\w\w+[\.]?\w\w+[\.][a-z][a-z]+$)", i)
     if result:
-        dict_email[i] = True  # print({result[0]: "True"})
+        dict_email[i] = True
     else:
-        dict_email[i] = False  # print("Wrong email")
+        dict_email[i] = False
 
 print(dict_email)
