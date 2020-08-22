d5a5a749874f48c87acee351e01fec79e3917bb7
Aleksey <melnikkov@gmail.com>
2020-08-22 13:32:13 +0300

    -

--- Training/VerifyEmail.py
+++ Training/VerifyEmail.py
@@ -3,11 +3,12 @@ import re
 mail = input("Input email: ").split(", ")
 
 dict_email = {}
+
 for i in mail:
+    dict_email[i] = False
+
     result = re.findall(r"(^\w\w+[@]\w\w+[\.]?\w\w+[\.][a-z][a-z]+$)", i)
     if result:
         dict_email[i] = True
-    else:
-        dict_email[i] = False
 
 print(dict_email)
--- Training/any.py
+++ Training/any.py
@@ -14,7 +14,7 @@
 # print(letter_counts)
 
 
-text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget tempus est. Phasellus sit amet 
+'''text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget tempus est. Phasellus sit amet 
 tristique neque. Sed luctus mi ut nisi suscipit placerat. Nunc nec diam dapibus, fermentum risus ut, ultrices orci. 
 Integer non magna molestie nibh dapibus tincidunt. Quisque quis est quam. Sed dictum mi sit amet magna pretium blandit. 
 Nulla tortor turpis, maximus vitae lobortis quis, varius sed metus. Nullam at congue metus. Pellentesque scelerisque, dui 
@@ -36,4 +36,22 @@ needLetter = l[len(l) - 2 : -1]
 
 for item in lt:
     if item[0] == needLetter[0]:
-        print(item[1])
+        print(item[1])'''
+
+list_email = input("Enter emails: ").split(", ")
+dict_email = {}
+
+for i in list_email:
+    result = False
+
+    if "@" in i and i.count("@") >= 1:
+        name = i.split("@")[0]
+        if len(name) >= 2:
+            domen = i.split("@")[1]
+            if "." in domen:
+                if len(domen.split(".")[0]) >= 2:
+                    result = True
+
+    dict_email[i] = result
+
+print(dict_email)
